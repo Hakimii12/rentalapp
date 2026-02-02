5bc7a5ed084b5c4316dbb2bd95a17fececfb6458
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2026-02-03 02:51:48 +0300

    ######

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+// ROUTE IMPORT 
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet.crossOriginOpenerPolicy({ policy: "same-origin"}));
+app.use(morgan("common"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }))
+app.use(cors());
+// ROUTES   
+app.get("/",(req,res)=>{
+    res.send("this is home page")
+})
+// SERVER
+const port = process.env.PORT || 3002;
+app.listen(port,()=>{
+   console.log(`Server is running on port http://localhost:${port}/`)
+})
